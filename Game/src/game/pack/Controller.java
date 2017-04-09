8039f4547460ca5c62f8b0996adbab6b4d6a0c1f
djatel <djatel@gmail.com>
2017-04-09 14:07:11 +0300

    com

--- Game/src/game/pack/Controller.java
+++ Game/src/game/pack/Controller.java
@@ -30,12 +30,20 @@ public class Controller implements Initializable {
     @FXML
     private ImageView imageView1;
 
+    @FXML
+    private ImageView image1;
+
+    @FXML
+    private ImageView image2;
+
+    @FXML
+    private ImageView image3;
+
+    private ArrayList<String> imageFiles;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//        File file = new File("Game/src/images/test_image.png");
-//        Image image = new Image(file.toURI().toString());
-//        imageView1.setImage(image);
+        loadImages("Game/src/images/");
     }
 
     @FXML
@@ -46,31 +54,29 @@ public class Controller implements Initializable {
         outputLabel2.setText(game.generate());
         outputLabel3.setText(game.generate());
 
-        ArrayList<String> imageFiles = new ArrayList<>();
-        File directory = new File("Game/src/images");
+        image1.setImage(new Image(new File(randomImageFilename()).toURI().toString()));
+        image2.setImage(new Image(new File(randomImageFilename()).toURI().toString()));
+        image3.setImage(new Image(new File(randomImageFilename()).toURI().toString()));
+
+    }
+
+    private void loadImages(String directoryPath) {
+        imageFiles = new ArrayList<>();
+
+        File directory = new File(directoryPath);
 
         File[] files = directory.listFiles();
         for (File f : files) {
-            imageFiles.add(f.getName());
+            imageFiles.add(f.getPath());
         }
-        System.out.println(imageFiles);
+        System.out.println("Images loaded: " + imageFiles);
+    }
+
+    private String randomImageFilename() {
         int imageRandomNumber = (int) (Math.random() * imageFiles.size());
         String imageRandomFilename = imageFiles.get(imageRandomNumber);
         System.out.println(imageRandomFilename); // random
-
-        //imageView1.setImage(new Image("Game/src/images/test_image.png"));
-
-        File file = new File("Game/src/images/test_image.png");
-        Image image = new Image(file.toURI().toString());
-        imageView1.setImage(image);
+        return imageRandomFilename;
     }
-
-//    private Image loadRandomImages() {
-//        int countImages = images.size();
-//        int imageNumber = (int) (Math.random() * countImages);
-//
-//        String image = images.get(imageNumber);
-//        return new Image(image);
-//    }
 
 }
