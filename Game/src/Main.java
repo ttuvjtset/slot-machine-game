ddd25bd772c555230f0c852fcd565590105baa5e
djatel <djatel@gmail.com>
2017-04-09 12:03:00 +0300

    com

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,10 +1,17 @@
+import java.util.Random;
+
 /**
  * Created by root on 09.04.2017.
  */
 public class Main {
     public static void main(String[] args) {
+        String[] reel = {"Joker", "Banana", "Lemon", "Orange", "Apple", "Melon"};
+        System.out.println(getRandom(reel));
 
+    }
 
-
+    public static String getRandom(String[] array) {
+        int randomItem = new Random().nextInt(array.length);
+        return array[randomItem];
     }
 }
